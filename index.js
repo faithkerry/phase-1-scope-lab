4dc1764ba89d052603c5ea09afc853fa24f09649
faith-kerry <kerubofaith944@gmail.com>
2025-09-19 13:38:45 +0300

    completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// declare customerName to be bob in global scope
+var customerName = "bob";
+
+// modifies the customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// setBestCustomer
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// overwrites the best customer
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// changeLeastFavoriteCustomer
+const leastFavoriteCustomer = "someone";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new customer"; // this will throw an error because it's const
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
+
